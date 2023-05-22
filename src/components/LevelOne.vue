4ffac562c934e5576e6ef0cea923dd3dc6da7f03
Baptiste <noye.baptiste@orange.fr>
2023-05-22 14:29:32 +0200

    data file created and component LevelOne displays data

---
+++ src/components/LevelOne.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import data from "../../data.json";
+export default {
+  setup() {
+    const collaborateurs = data;
+    return {
+      collaborateurs,
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>Bienvenue dans le niveau 1</h2>
+  <div v-for="collab in collaborateurs" :key="collab.id">
+    <p>{{ collab.name.first }} {{ collab.name.last }}</p>
+    <img :src="collab.image" />
+  </div>
+</template>
+
+<style>
+img {
+  width: 150px;
+  height: 150px;
+}
+</style>
