aad468c001ee5d833b7549618413a52d045abe30
Baptiste <noye.baptiste@orange.fr>
2023-05-23 17:22:51 +0200

    the correct answer index position is random

--- src/components/LevelOne.vue
+++ src/components/LevelOne.vue
@@ -1,72 +1,54 @@
-<script lang="ts">
+<script setup>
 import data from "../../data.json";
-import {ref, computed, watch} from "vue";
+import { ref, computed, watch } from "vue";
 
-export default {
-  setup() {
-    const collaborateurs = data;
-    const max = collaborateurs.length;
+const collaborators = data;
+const max = collaborators.length;
 
-    let rand = ref(Math.floor(Math.random() * max));
-    const randCollab = computed(() => collaborateurs[rand.value]);
-    // console.log("id photo affichée :", randCollab.value.id);
+let rand = ref(Math.floor(Math.random() * max));
+const randCollab = computed(() => collaborators[rand.value]);
 
-    const randIds = computed(() => {
-      let selected = [];
-      selected.push(randCollab.value.id - 1);
-      for (let i = 0; i < 3; i++) {
-        let oneRandomIndex = Math.floor(Math.random() * max);
-        while (selected.includes(oneRandomIndex)) {
-          oneRandomIndex = Math.floor(Math.random() * max);
-        }
-        selected = [...selected, oneRandomIndex];
-      }
-      // console.log(selected);
+const randIds = computed(() => {
+  let selected = [];
+  selected.push(randCollab.value.id - 1);
+  for (let i = 0; i < 3; i++) {
+    let oneRandomIndex = Math.floor(Math.random() * max);
+    while (selected.includes(oneRandomIndex)) {
+      oneRandomIndex = Math.floor(Math.random() * max);
+    }
+    selected = [...selected, oneRandomIndex];
+  }
 
-      return selected;
-    });
+  return _.shuffle(selected);
+});
 
-    const reload = () => {
-      selectedName.value = "";
-      victory.value = false;
-      failure.value = false;
-      const oldRand = rand.value;
-      while (rand.value === oldRand) {
-        rand.value = Math.floor(Math.random() * max);
-      }
-    };
-
-    const selectedName = ref("");
-
-    const victory = ref(false);
-    const failure = ref(false);
-
-    watch(selectedName, () => {
-      console.log("sélectionné", selectedName);
-      console.log("photo affichée", randCollab.value.id);
-
-      const selectedId = parseInt(selectedName.value, 10) + 1;
-      if (selectedId.toString() === randCollab.value.id.toString()) {
-        victory.value = true;
-        failure.value = false;
-      } else {
-        failure.value = true;
-        victory.value = false;
-      }
-    });
-
-    return {
-      collaborateurs,
-      max,
-      randCollab,
-      reload,
-      randIds,
-      selectedName,
-      victory,
-      failure,
-    };
-  },
+const reload = () => {
+  selectedName.value = "";
+  victory.value = false;
+  clicked.value = false;
+  const oldRand = rand.value;
+  while (rand.value === oldRand) {
+    rand.value = Math.floor(Math.random() * max);
+  }
 };
+
+const handleClick = () => {
+  clicked.value = true;
+};
+
+const selectedName = ref("");
+
+const victory = ref(false);
+const clicked = ref(false);
+
+watch(selectedName, () => {
+  const selectedId = parseInt(selectedName.value, 10) + 1;
+  if (selectedId.toString() === randCollab.value.id.toString()) {
+    victory.value = true;
+  } else {
+    victory.value = false;
+  }
+});
 </script>
 
 <template>
@@ -76,28 +58,27 @@ export default {
 
     <!--COMPOSANT AVEC EMIT/PROPS -->
     <div>
-      <img :src="randCollab.image" alt=""/>
+      <img :src="randCollab.image" alt="" />
     </div>
-    <div v-for="(nameId, i) in randIds" :key="i">
-      <input type="radio" :id="nameId" :value="nameId" v-model="selectedName"/>
-      <label for="nameId">{{ collaborateurs[nameId].name.first }}</label>
+    <div v-for="(nameId, i) in randIds" :key="i" @click="handleClick">
+      <input
+        type="radio"
+        :id="i.toString()"
+        :value="nameId"
+        v-model="selectedName"
+      />
+      <label :for="i.toString()">{{ collaborators[nameId].name.first }}</label>
     </div>
     <!-- END COMPOSANT -->
 
-
-
-    <div class="" v-if="selectedName">
+    <div class="" v-if="clicked">
       <p class="victory" v-if="victory">Gagné !</p>
-      <p class="failure" v-else-if="failure">Essaye encore...</p>
+      <p class="failure" v-else>Essaye encore...</p>
     </div>
-
-
   </div>
-
 </template>
 
 <style lang="scss">
-
 img {
   width: 150px;
   height: 150px;
