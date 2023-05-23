66e681482348cc48f9615a23201760b5a55c24ab
lecappa <lecappa@gmail.com>
2023-05-23 11:07:10 +0200

    Refacto

--- src/components/LevelOne.vue
+++ src/components/LevelOne.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
 import data from "../../data.json";
-import { ref, computed, watch } from "vue";
+import {ref, computed, watch} from "vue";
+
 export default {
   setup() {
     const collaborateurs = data;
     const max = collaborateurs.length;
+
     let rand = ref(Math.floor(Math.random() * max));
     const randCollab = computed(() => collaborateurs[rand.value]);
     // console.log("id photo affichée :", randCollab.value.id);
@@ -25,6 +27,9 @@ export default {
     });
 
     const reload = () => {
+      selectedName.value = "";
+      victory.value = false;
+      failure.value = false;
       const oldRand = rand.value;
       while (rand.value === oldRand) {
         rand.value = Math.floor(Math.random() * max);
@@ -65,20 +70,34 @@ export default {
 </script>
 
 <template>
-  <h2>Bienvenue dans le niveau 1</h2>
-  <button @click="reload">Recharger</button>
   <div>
-    <img :src="randCollab.image" />
+    <h2>Bienvenue dans le niveau 1</h2>
+    <button @click="reload">Recharger</button>
+
+    <!--COMPOSANT AVEC EMIT/PROPS -->
+    <div>
+      <img :src="randCollab.image" alt=""/>
+    </div>
+    <div v-for="(nameId, i) in randIds" :key="i">
+      <input type="radio" :id="nameId" :value="nameId" v-model="selectedName"/>
+      <label for="nameId">{{ collaborateurs[nameId].name.first }}</label>
+    </div>
+    <!-- END COMPOSANT -->
+
+
+
+    <div class="" v-if="selectedName">
+      <p class="victory" v-if="victory">Gagné !</p>
+      <p class="failure" v-else-if="failure">Essaye encore...</p>
+    </div>
+
+
   </div>
-  <div v-for="(nameId, i) in randIds" :key="i">
-    <input type="radio" id="nameId" :value="nameId" v-model="selectedName" />
-    <label for="nameId">{{ collaborateurs[nameId].name.first }}</label>
-  </div>
-  <p class="victory" v-if="victory">Gagné !</p>
-  <p class="failure" v-else-if="failure">Essaye encore...</p>
+
 </template>
 
-<style>
+<style lang="scss">
+
 img {
   width: 150px;
   height: 150px;
